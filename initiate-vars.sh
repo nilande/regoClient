29a7190d9ed8041f426ebd439f1be108771c9659
Anders Nilsson <anders.nilsson@gmail.com>
2016-03-09 21:05:51 +0100

    Cleanup

--- initiate-vars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PATH=$PATH:/nfs/src/OpenWrt-SDK-15.05-ar71xx-generic_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/bin
-export PATH
-STAGING_DIR=/nfs/src/OpenWrt-SDK-15.05-ar71xx-generic_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/bin
-export STAGING_DIR
